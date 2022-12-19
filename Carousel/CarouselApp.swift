c56c698c5f974041a3cd156814004ac28a061b63
Cameron <caslash@bsu.edu>
2022-12-19 05:38:54 -0500

    Initial Commit

---
+++ Carousel/CarouselApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarouselApp.swift
+//  Carousel
+//
+//  Created by Cameron Slash on 12/19/22.
+//
+
+import SwiftUI
+
+@main
+struct CarouselApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
